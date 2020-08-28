060f78864f79339e7e13051fa3ba19ae8162a784
Eric Barndollar <eric@EricWBarndollar.com>
2020-08-28 15:26:37 -0400

    Initial Angular Material library + style setup.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from '@angular/core';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {FontAwesomeModule} from '@fortawesome/angular-fontawesome';
 
 import {AppRoutingModule} from './app-routing.module';
@@ -9,6 +10,7 @@ import {AppComponent} from './app.component';
   declarations: [AppComponent],
   imports: [
     AppRoutingModule,
+    BrowserAnimationsModule,
     BrowserModule,
     FontAwesomeModule,
   ],
