e20e45873d5c61a6d73547c95672092356f583f7
Eric W. Barndollar <eric@EricWBarndollar.com>
2020-08-29 16:19:24 -0400

    Access ArghoEditorModel from EditorComponent.
    Just to prove that basics are working.

--- src/app/features/editor/editor.component.ts
+++ src/app/features/editor/editor.component.ts
@@ -1,8 +1,23 @@
+import {TuningDataService} from 'src/app/infra/tuning-data/tuning-data.service';
+
 import {Component} from '@angular/core';
+import {
+  ArghoEditorModel,
+  TuningMetadataSnapshot,
+} from '@arghotuning/argho-editor';
 
 @Component({
   selector: 'app-editor',
   templateUrl: './editor.component.html',
   styleUrls: ['./editor.component.scss'],
 })
-export class EditorComponent { }
+export class EditorComponent {
+  tuningMetadata!: TuningMetadataSnapshot;
+
+  constructor(data: TuningDataService) {
+    // Note: Subscription is always called synchronously.
+    data.model.tuningMetadata().subscribe(metadata => {
+      this.tuningMetadata = metadata;
+    });
+  }
+}
